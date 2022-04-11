dc48057a20541ae7d3d640d60e53e3ed127fc577
quandareleast <buikhacminhquan@hotmail.com>
2022-04-11 02:13:58 -0400

    adding productdetails

---
+++ TBayEat_GroupProject/ProductDetails.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Master/FrontEnd.Master" AutoEventWireup="true" CodeBehind="ProductDetails.aspx.cs" Inherits="TBayEat_GroupProject.ProductDetails" %>
+
+<asp:Content ID="productDetailHeader" ContentPlaceHolderID="head" runat="server">
+    <link href="Content/bootstrap.css" rel="stylesheet"/>
+    <link href="./Style/Main.css" rel="stylesheet" type="text/css" />
+    <link href="./Style/homepage.css" rel="stylesheet" type="text/css" />
+</asp:Content>
+<asp:Content ID="detailMainPage" ContentPlaceHolderID="body" runat="server">
+        <div class="bannerWrapper">
+            <div>
+                <div>
+                    <h2 class="title">Authentic local food in Tbay</h2>
+                    <p class="subtitle">TBayEat is a courier service in which authentic home cook food is delived to a customer</p>
+                </div>
+                <div class="searchWrapper">
+                    <form class="form-inline" method="POST">
+                        <input class="form-control mr-sm-2 searchBox" type="search" placeholder="Search food you love" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0 searchBtn" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+            <div>
+                <img src="./images/cuate.png" style="clip: rect(auto, auto, auto, 20px)" />
+            </div>
+        </div>
+    <asp:FormView ID="productDetail" ItemType="TBayEat_GroupProject.ProductInfo" runat="server" SelectMethod="productDetail_GetItem" RenderOuterTable="false">
+        <ItemTemplate>
+            <h1><%#:Item.Name %></h1>
+        </ItemTemplate>
+    </asp:FormView>
+</asp:Content>
+
---
+++ TBayEat_GroupProject/ProductDetails.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.ModelBinding;
+
+namespace TBayEat_GroupProject
+{
+
+    public class ProductInfo
+    {
+        public string Name { get; set; }
+        public string Itemid { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Price { get; set; }
+        
+        public ProductInfo(string itemId, string name, string description, string image, string price)
+        {
+            Name = name;
+            Itemid = itemId;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+    public partial class ProductDetails : System.Web.UI.Page
+    {
+        private String productId;
+        private String userId;
+        public ProductInfo productInformation;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["productID"]!=null)
+            {
+                productId = Request.QueryString["productID"];
+                if (Session["userId"]!=null)
+                {
+                    userId = Session["userId"].ToString();
+                }
+
+
+            } else
+            {
+                Response.Redirect("HomePage.aspx");
+            }
+
+        }
+
+        // The id parameter should match the DataKeyNames value set on the control
+        // or be decorated with a value provider attribute, e.g. [QueryString]int id
+        public ProductInfo productDetail_GetItem([QueryString("productID")] int? id)
+        {
+            if (id.HasValue && id > 0)
+            {
+                string strConnection = ConfigurationManager.ConnectionStrings["TBayEatConnection"].ConnectionString;
+                SqlConnection sqlConnection = new SqlConnection(strConnection);
+                String SQLQuery = "select * from dbo.Items where ItemId="+ id;
+                SqlCommand command = new SqlCommand(SQLQuery, sqlConnection);
+                SqlDataReader dr;
+                sqlConnection.Open();
+                dr = command.ExecuteReader();
+                while(dr.Read())
+                {
+                    productInformation = new ProductInfo(dr["ItemId"].ToString(), dr["Name"].ToString(), dr["Description"].ToString(), dr["Image"].ToString(), dr["Price"].ToString());
+                }
+                dr.Close();
+                return productInformation;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
